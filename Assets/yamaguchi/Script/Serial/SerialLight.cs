1a08b623db72b6abdd32896da8aa063c89d98902
tai <taikitpmiita@icloud.com>
2021-07-10 14:15:20 +0900

    [add]オーディオマネージャーとシリアル通信のスクリプトを追加
    オーディオマネージャーのシングルトン実装とシリアル通信用のスクリプトの追加

---
+++ Assets/yamaguchi/Script/Serial/SerialLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerialLight : MonoBehaviour
+{
+
+    public SerialHandler serialHandler;
+    public Text text;
+
+    void Start()
+    {
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDataReceived(string message)
+    {
+        try
+        {
+            text.text = message;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
