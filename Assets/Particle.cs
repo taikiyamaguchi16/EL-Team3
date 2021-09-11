545b6d936071e87ec428add3c0c4395c4e11c8ef
haradakaichi714 <kit714@icloud.com>
2021-09-11 11:08:42 +0900

    集中線プレハブ

---
+++ Assets/Particle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    ParticleDestroy();
+        //}
+    }
+    public void ParticleDestroy()
+    {
+        this.transform.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+    }
+    public void ParticleStart()
+    {
+        this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+}
---
+++ Assets/harada/particle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ParticleDestroy();
+        }
+    }
+    public void ParticleDestroy()
+    {
+        this.transform.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+    }
+}
